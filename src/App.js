95b2ec0416124c388f995df59aaa1d13d8266d56
Wheeler Weise <wheeler.weise@gmail.com>
2019-06-24 16:43:39 -0500

    Issue is now reproduced.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
 
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+  button: {
+    margin: theme.spacing(1),
+  },
+  input: {
+    display: 'none',
+  },
+}));
+
 class App extends React.Component {
 
   render() {
+
+    let classes = useStyles();
+
     return (
-      <Button variant="contained" color="primary">
+      <Button variant="contained" color="primary" className={classes.button}>
         Hello World
       </Button>
     );
